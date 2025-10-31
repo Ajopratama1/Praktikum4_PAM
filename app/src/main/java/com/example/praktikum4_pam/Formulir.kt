e99d4a97a2d5bb288a2566063e67fb872dfddb5c
Ajopratama1 <a.ajopratama01@gmail.com>
2025-10-31 22:47:10 +0700

    Add basic RegistrationForm composable

--- app/src/main/java/com/example/praktikum4_pam/Formulir.kt
+++ app/src/main/java/com/example/praktikum4_pam/Formulir.kt
@@ -1,19 +1,28 @@
 package com.example.praktikum4_pam
 
-
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.ImeAction
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.example.praktikum4_pam.ui.theme.Praktikum4_PAMTheme
+
+@Composable
+fun RegistrationForm() {
+    var namaLengkap by remember { mutableStateOf("") }
+    var jenisKelamin by remember { mutableStateOf("") }
+    val jenisKelaminOptions = listOf("Laki-laki", "Perempuan")
+    var statusPerkawinan by remember { mutableStateOf("") }
+    val statusPerkawinanOptions = listOf("Janda", "Lajang", "Duda")
+    var alamat by remember { mutableStateOf("") }
+
